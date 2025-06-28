1a452847356261cb3d7675b78d279198ec0bc543
dafferin <dafff23318@gmail.com>
2025-06-28 11:13:46 +0530

    Updated title and image

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/portfolio/", // 🔴 This is what you need to add
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
